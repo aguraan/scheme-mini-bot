a90cfa9f0a93c7673b29b5d667c722f41ec6ecad
aguraan <agura.iv@gmail.com>
2020-09-11 11:11:09 +0300

    fix

--- app/index.js
+++ app/index.js
@@ -20,7 +20,6 @@ if (process.env.NODE_ENV === 'production') {
 
     console.info('Bot launched. mode: Webhook-Docker')
 } else {
-<<<<<<< HEAD
     const cert = fs.readFileSync('/etc/letsencrypt/live/scheme.com.ua/cert.pem')
     const key = fs.readFileSync('/etc/letsencrypt/live/scheme.com.ua/privkey.pem')
     const tlsOptions = { key, cert, ca: [ cert ] }
@@ -29,14 +28,6 @@ if (process.env.NODE_ENV === 'production') {
     bot.telegram.setWebhook(process.env.WEB_HOOKS_SECRET_URL, {
         source: cert
     })
-=======
-    const tlsOptions = {
-        key: fs.readFileSync('/etc/letsencrypt/live/scheme.com.ua/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/scheme.com.ua/cert.pem')
-    }
-
-    bot.telegram.setWebhook(process.env.WEB_HOOKS_SECRET_URL)
->>>>>>> 2d1cc768bab2dee3816057dd61257f18c6325629
     bot.startWebhook(process.env.WEB_HOOKS_PATH, tlsOptions, process.env.PORT)
 
     bot.telegram.getWebhookInfo()
